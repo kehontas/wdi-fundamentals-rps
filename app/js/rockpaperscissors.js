e4b869ec4fcada9742b4507b171f37cee86605cb
KEHONTAS ROWE <kehontasrowe@KEHONTASs-MacBook-Pro.local>
2015-11-09 19:06:52 -0800

    Added the terms of a winning game ie player, computer, or tie

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -38,10 +38,32 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
+     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+
+    //https://repl.it/9nH/1742
+    if (playerMove==='rock' && computerMove==='scissors'){
+        winner = 'player';
+    else if (computerMove==='rock' && playerMove==='scissors'){
+        winner = 'computer';
+    }
+    if (playerMove==='scissors' && computerMove==='paper'){
+        winner = 'player';
+    }
+    else if (computerMove==='scissors' && playerMove==='paper'){
+        winner = 'computer';
+    }
+    if (playerMove==='paper' && computerMove==='rock'){
+        winner = 'player';
+    }
+    else if (computerMove==='paper' && playerMove==='rock'){
+        winner = 'computer';
+    }
+    else (playerMove===computerMove)
+        winner = 'tie';
+   
     return winner;
 }
 
